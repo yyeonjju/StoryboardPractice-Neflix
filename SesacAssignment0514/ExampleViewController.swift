d73eb2e77a87d8f2dc440aa2343fe5ad2b5a1c0e
yyeonjju <llilyha123@gmail.com>
2024-05-17 14:37:26 +0900

    Add : Add ExampleViewController file

---
+++ SesacAssignment0514/ExampleViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ExampleViewController.swift
+//  SesacAssignment0514
+//
+//  Created by 하연주 on 5/17/24.
+//
+
+import UIKit
+
+class ExampleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
